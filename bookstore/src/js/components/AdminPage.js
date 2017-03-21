b475223d382b3772810190d5241455128b146406
U-DESKTOP-6HUBN10\Roxana <g.roxanag@yahoo.com>
2017-03-21 10:36:54 +0200

    second commit

--- bookstore/src/js/components/AdminPage.js
+++ bookstore/src/js/components/AdminPage.js
@@ -2,26 +2,26 @@ import React , {PropTypes} from 'react';
 
 const AdminPage = ({booksList, addNewBook}) => {
  	return (
- 		<div>
-          <h3>Add a new book</h3>
-          <form onSubmit={e => {
-            e.preventDefault();
-            addNewBook(e);
+ 		 <div>
+        <h3>Add a new book</h3>
+        <form onSubmit={e => {
+                  e.preventDefault();
+                  addNewBook(e);
         }}>
-        <div className="form-group" >
-          <input type="text" name="title" placeholder="Book Name" required="required" />
-        </div>
-        <div className="form-group">
-          <input type="text" name="author" placeholder="Author" required="required" />
-        </div>
-        <div className="form-group">
-          <input type="text" name="price" placeholder="Price" required="required"/>
-        </div>
-        <div className="form-group">
-          <button type="submit" className="btn btn-success" >Submit </button>
-        </div>
+          <div className="form-group" >
+            <input type="text" name="title" placeholder="Book Name" required="required" />
+          </div>
+          <div className="form-group">
+            <input type="text" name="author" placeholder="Author" required="required" />
+          </div>
+          <div className="form-group">
+            <input type="text" name="price" placeholder="Price" required="required"/>
+          </div>
+          <div className="form-group">
+            <button type="submit" className="btn btn-success" >Submit </button>
+          </div>
         </form>
-        </div> )
+      </div> )
 };
 
 AdminPage.PropTypes = {
